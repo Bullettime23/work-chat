e54d9d95d4a2da58ddffcee5e316f445828a1a5c
Bullettime23 <letsgofrontend@gmail.com>
2024-04-10 09:28:41 +0500

    add: added loading skeletons fro components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 'use client';
-import { db } from '@/lib/db';
 import { signOut } from 'next-auth/react';
 
-export default async function Home() {
-	await db.set('Check', 'Checked!');
-
+export default function Home() {
 	return <button onClick={() => signOut()}>SignOut</button>;
 }
